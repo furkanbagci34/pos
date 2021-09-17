7851bd2bb9cc69c898b5c1f8acce9d984e263813
furkanbagci34 <62967565+furkanbagci34@users.noreply.github.com>
2021-09-17 16:47:04 +0300

    .

---
+++ www/plugins/admin/param/pos/param.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from '../../../../admin/lib/app.js';
+
+export default class Param extends React.Component
+{
+    constructor()
+    {
+        super()
+    }
+    render()
+    {
+        return(<div>SELAM</div>)
+    }
+}
